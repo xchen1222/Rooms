a6d4e1db1f8e969a79fe0ab3880b42c45df565e1
sajkldn <xchen7640@bths.edu>
2017-12-18 09:19:14 -0500

    sdwa

--- Rooms/src/Monster.java
+++ Rooms/src/Monster.java
@@ -5,6 +5,22 @@ public class Monster extends Person {
 	{
 		super (name , xLoc, yLoc, health) ; 
 	}
-		
+	public void  setNameMonster(String name)
+	{
+		this.name = name ; 
+	}
+	public String getNameMonster() 
+	{
+		return name; 
+	}
+	public void sethealthMonster(int health) 
+	{
+		this.health = health;
+	}
+	public int getHealthMonster()
+	{
+		return health ; 
+	}
+	
 	
 }
